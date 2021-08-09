eaa330dea0c15b6ccb6ff6c0d839bca0bc592d32
Jack Penzer <jackpenzer@Jacks-MacBook-Air.local>
2021-08-09 13:17:33 +0100

    removed ParseR dependency added additional control flow for limpiar_df - + optionally remove punctuation and removed name cleaning step - advisable to do this separately and before anything else.

--- R/limpiar_df.R
+++ R/limpiar_df.R
@@ -8,6 +8,7 @@
 #' @param df Data Frame or Tibble Object
 #' @param text_var The text variable/character vector
 #' @param remove_retweets Whether or not to remove retweets, default is to remove
+#' @param remove_punctuation Whether or not to remove punctuation (and then clean whitespace)
 #' @importFrom magrittr %>%
 #' @return The original Data Frame with the text variable cleaned
 #' @export
@@ -15,25 +16,37 @@
 #' @examples
 #' print("hello world")
 
-limpiar_df <- function(df, text_var, remove_retweets = TRUE){
+limpiar_df <- function(df, text_var, remove_retweets = TRUE, remove_punctuation = FALSE){
   .col = rlang::enquo(text_var)
 
   df <- df %>%
-    janitor::clean_names()%>%
     janitor::remove_empty(which = "rows")%>%
-    limpiar_duplicates(!!rlang::enquo(text_var))%>%
     #tidy evaluation takes column input, quotes + unquotes
     dplyr::mutate(!!paste0("", rlang::quo_name(.col)) := tolower(!!rlang::enquo(text_var)))%>%
     dplyr::mutate(!!paste0("", rlang::quo_name(.col)) := limpiar_accents(!!rlang::enquo(text_var)))%>%
-    limpiar_url(!!rlang::enquo(text_var))%>%
-    limpiar_spaces(!!rlang::enquo(text_var))
+    limpiar_url(!!rlang::enquo(text_var))
+
 
   #Option to not remove retweets
-  if(remove_retweets == TRUE){
-    df %>%
+  if(remove_retweets){
+    df <- df %>%
       dplyr::filter(!stringr::str_detect(!!rlang::enquo(text_var), "\\brt\\b"))
+    df
   }else{
     df
   }
+
+  #Option to remove punctuation
+  if (remove_punctuation){
+    df <-df %>%
+      dplyr::mutate(!!paste0("", rlang::quo_name(.col)) := tm::removePunctuation(!!rlang::enquo(text_var)))
+  }else{
+    df
+  }
+
+  #Now remove excess white spaces and duplicates
+  df %>%
+    limpiar_spaces(!!rlang::enquo(text_var))%>%
+    limpiar_duplicates(!!rlang::enquo(text_var))
 }
 
