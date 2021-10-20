e5e722c133487066b0f1083d37e8f31d7e7b2ad4
Jack Penzer <jackpenzer@MacBook-Air.local>
2021-10-20 10:41:02 +0100

    added additional shorthands to limpiar_shorthands

--- R/limpiar_shorthands.R
+++ R/limpiar_shorthands.R
@@ -15,18 +15,22 @@
 limpiar_shorthands <- function(text_var, spaces_as_underscores = FALSE){
 
   shorthands <- c("\\bporq\\b","\\btqm\\b", "\\btq\\b", "\\bpq\\b", "\\bxq\\b", "\\bq\\b", "\\bk\\b", "\\bxk\\b",
-                  "\\bpk\\b", "\\bxfa\\b", "\\bxa q\\b", "mxo", "bst", "\\btam\\b", "pti",
-                  "\\bkn\\b", "ntnc", "\\btonces\\b", "\\bgnl\\b", "\\bkyat\\b",
+                  "\\bpk\\b", "\\bxfa\\b", "\\bxa q\\b", "\\bmxo\\b", "\\bbst\\b", "\\btam\\b", "\\bpti\\b",
+                  "\\bkn\\b", "\\bntnc", "\\btonces\\b", "\\bgnl\\b", "\\bkyat\\b",
                   "\\bfin d\\b", "\\bKO\\b", "\\b(TQI|tqi)\\b", "\\b(TKI|tki)\\b",
                   "\\b(NPN|npn)\\b", "\\bvrd\\b", "\\bvdd\\b", "\\bntp\\b", "\\b(GPI|gpi)\\b", "\\bslds\\b", "\\bctm\\b",
-                  "\\bgrax\\b", "\\bwn\\b")
+                  "\\bgrax\\b", "\\bwn\\b", "\\basdc\\b", "\\b100pre\\b", "\\b(k|q) aces\\b", "\\bsbs\\b",
+                  "\\bvns\\b", "\\baora\\b", "\\bbn\\b", "\\bnx\\b","\\bcdo\\b", "\\bdim\\b",
+                  "\\bdcr\\b", "\\bkntm\\b", "\\bnph\\b", "\\bre100\\b", "\\btvo\\b", "\\bweno\\b")
 
   shorthand_corrections <- c("\\bporque\\b", "te_quiero_mucho", "te_quiero", "porque", "porque", "que", "que",
                              "porque", "porque", "por_favor", "para_que", "mucho", "besitos",
                              "te_amo_mucho", "para_ti", "quien", "entonces", "entonces", "genial", "callate",
                              "fin_de_semana", "muerto", "tengo_que_irme", "tengo_que_irme",
                              "no_pasa_nada", "verdad", "verdad", "no_te_preocupes", "gracias_por_invitar",
-                             "saludos", "chinga_tu_madre", "gracias", "wuevon")
+                             "saludos", "chinga_tu_madre", "gracias", "wuevon", "a_salir_de_casa", "siempre",
+                             "que haces", "sabes", "vienes", "ahora", "bien", "buenas noches", "cuando", "dime",
+                             "decir", "cuentame", "no_puedo_hablar", "recien", "te_veo", "bueno")
   if(spaces_as_underscores){
     shorhand_corrections <- shorthand_corrections
   }else{
@@ -41,3 +45,4 @@ limpiar_shorthands <- function(text_var, spaces_as_underscores = FALSE){
                                         hash::values(my_hash),
                                         hash::keys(my_hash)))
 }
+
