40655df40d7120da15b53e8b134c19b93b8e9c1d
Jack Penzer <jack.penzer@sharecreative.com>
2022-11-23 15:05:04 +0000

    adding limpiar_pp_products function

---
+++ R/limpiar_pp_products.R
@@ -0,0 +1,26 @@
+#' Replace entities for the Peaks&Pit classifier
+#'
+#' @param df Data Frame or Tibble object
+#' @param text_var Name of your text variable
+#'
+#' @return Data Frame or Tibble object with text variable edited inline
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Example 1
+#'df %>% limpiar_pp_products(message)
+#' Example 2
+#'limpiar_pp_products(df, message)
+#' }
+#'
+limpiar_pp_products <- function(df, text_var){
+  entities <- LimpiaR::entities
+
+  strings <- entities$token
+  strings <- paste0(strings, collapse = "|")
+  replacement <- "product"
+
+  df %>%
+    dplyr::mutate({{text_var}} := stringr::str_replace_all({{text_var}}, strings, replacement))
+}
