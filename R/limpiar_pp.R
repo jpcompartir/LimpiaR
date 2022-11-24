238ab1368d7c9fee9016d75d84bb083b64f89b36
Jack Penzer <jack.penzer@sharecreative.com>
2022-11-24 16:35:40 +0000

    fix |apple in products, add more logic to companies.

--- R/limpiar_pp.R
+++ R/limpiar_pp.R
@@ -18,7 +18,7 @@ limpiar_pp_products <- function(df, text_var){
   entities <- LimpiaR::entities
 
   entities <- entities %>%
-    dplyr::filter(!token %in% c("\\bmicrosoft\\b","\\b|apple\\b", "\\bsamsung\\b", "\\bwhatsapp\\b", "\\bintel\\b", "\\bamazon\\b", "\\bsony\\b", "\\binstagram\\b"))
+    dplyr::filter(!token %in% c("\\bmicrosoft\\b","\\bapple\\b", "\\bsamsung\\b", "\\bwhatsapp\\b", "\\bintel\\b", "\\bamazon\\b", "\\bsony\\b", "\\binstagram\\b"))
 
   extras <- c("\\bsurface pro 8\\b", "\\bwin 11\\b", "\\bwin11\\b", "\\bsurface pro\\b")
 
@@ -49,9 +49,12 @@ limpiar_pp_products <- function(df, text_var){
 #' }
 limpiar_pp_companies <- function(df, text_var){
   companies <- c("\\bapple\\b", "\\bmicrosoft\\b", "\\bmsft\\b", "\\bnvidia\\b", "\\bsony\\b", "\\binstagram\\b",
-                 "\\bwhatsapp\\b", "duckduckgo", "\\bddg\\b", "\\bduck duck go\\b")
+                 "\\bwhatsapp\\b", "duckduckgo", "\\bddg\\b", "\\bduck duck go\\b", "\\bsamsung\\b", "\\blenovo\\b",
+                 "\\bdell\\b", "\\bfacebook\\b", "\\byoutube\\b", "\\bxbox\\b", "\\bspotify\\b", "\\bmozilla firefox\\b",
+                 "\\bmozilla\\b", "\\bfirefox\\b")
+  companies <- paste0(companies, collapse = "|")
   replacement <- "company"
 
   df %>%
-    dplyr::mutate({{text_var}} := stringr::str_replace_all({{text_var}}, companies))
+    dplyr::mutate({{text_var}} := stringr::str_replace_all({{text_var}}, companies, replacement))
 }
