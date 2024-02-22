6e59a12a22a7b9d4c4e62981cc122a6783bc8f59
Jack Penzer <jp1689@my.bristol.ac.uk>
2024-02-22 11:13:27 +0000

    Update limpiar_pos_import_model to send models to cache when possible, and if not tempdir()

--- R/limpiar_pos_import_model.R
+++ R/limpiar_pos_import_model.R
@@ -13,15 +13,26 @@
 #' pos_model <- limpiar_pos_import_model(language = "english")
 limpiar_pos_import_model <- function(language) {
 
+  # Set up the route to the model cache directory - we want to cache models where possible
+  model_cache_dir <- file.path(normalizePath(system.file(package = "LimpiaR")), "model_cache")
+
+  # Check the directory exists as it should've been created in .onLoad - if it's not, we'll just use a tempdir() - should circumvent potential problems in Virtual Machines like github actions etc.?
+  if(dir.exists(model_cache_dir)){
+    model_dir = model_cache_dir
+  } else {
+    message("Cannot identify model_cache directory in Library, saving model to tempdir()")
+    model_dir = tempdir()
+  }
+
   # stop if not statement, for language input being of string class
   stopifnot(rlang::is_string(language))
 
   # save model to a temporary directory and don't overwrite unless models not already in the temp directory
   model <- udpipe::udpipe_download_model(language = language,
-                                         model_dir = tempdir(),
+                                         model_dir = model_dir,
                                          overwrite = FALSE)
 
-  # load model from the temp directory
+  # load model from the directory
   model_loaded <- udpipe::udpipe_load_model(file = model$file)
 
   return(model_loaded)
